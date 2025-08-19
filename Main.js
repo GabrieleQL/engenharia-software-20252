b49264e6b5de7c4285aaf8f47287d77bcdfdc0d4
Gabriele Lapischies <gabrielelapischies@gmail.com>
2025-08-19 10:53:29 -0300

    Add saudação function

--- Main.js
+++ Main.js
@@ -5,3 +5,9 @@ console.log("My Lord is Marvelous!")
 for (let i = 0; 1 < 5; 1++){
     console.log("Olá mundo ", i, "° !")
 }
+
+function saudacaoPessoal(nome){
+    console.log("Olá ", nome, "!")
+}
+
+saudacaoPessoal("Avelina")
