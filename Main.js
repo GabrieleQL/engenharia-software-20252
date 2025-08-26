294f84e3443e27aabe437a5a27f87a54f60c9dee
Gabriele Lapischies <gabrielelapischies@gmail.com>
2025-08-26 10:26:43 -0300

    Update loop counter

--- Main.js
+++ Main.js
@@ -2,13 +2,13 @@ console.log("Deus é Fiel! 😁")
 
 console.log("My Lord is Marvelous!")
 
-for (let i = 0; 1 < 5; 1++){
+for (let i = 0; 1 < 7; 1++){
     console.log("Olá mundo ", i, "° !")
 }
 
 function saudacaoPessoal(nome){
     console.log("Olá ", nome, "!")
-    console.log("Bem-vinda ao nosso sistema")
+    console.log("Bem-vinda ao nosso querido sistema")
 }
 
 saudacaoPessoal("Avelina")
